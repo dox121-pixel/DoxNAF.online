fe75cb2fa899e502e4cd08e42a4995f01e1de3e4
dox121-pixel <doxgamer121@gmail.com>
2026-02-16 20:43:19 +0100

    Add files via upload
    Scroll logic 1.0

--- script.js
+++ script.js
@@ -82,3 +82,14 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+// SHRINKING HEADER
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
+});
